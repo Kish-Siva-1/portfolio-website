4de63f31b23ab5999b89a01e50fa2d4d5092b745
Kish-Siva-1 <rodes88@msn.com>
2020-01-22 18:08:38 +0100

    will remove additional social media icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'; 
 import './App.css';
 import { Navbar, Home, About, TechStack, Portfolio, Blogs, Contact, CopyRight } from "./components/index";
-// import { Navbar } from "./components/NavBar/navBar";
 
 class App extends React.Component {
   render() {
--- src/components/Home/homePage.js
+++ src/components/Home/homePage.js
@@ -11,20 +11,6 @@ export const Home = () => {
         <p className="masthead-subheading font-weight-light mb-0">
           I'm a full stack engineer.
         </p>
-        {/* <div className="col-lg-4 mb-5 mb-lg-0 ">
-          <a className="btn btn-outline-light btn-social mx-1" href="#">
-            <i className="fab fa-fw fa-facebook-f"></i>
-          </a>
-          <a className="btn btn-outline-light btn-social mx-1" href="#">
-            <i className="fab fa-fw fa-twitter"></i>
-          </a>
-          <a className="btn btn-outline-light btn-social mx-1" href="#">
-            <i className="fab fa-fw fa-linkedin-in"></i>
-          </a>
-          <a className="btn btn-outline-light btn-social mx-1" href="#">
-            <i className="fab fa-fw fa-dribbble"></i>
-          </a>
-        </div> */}
       </div>
     </header>
 
