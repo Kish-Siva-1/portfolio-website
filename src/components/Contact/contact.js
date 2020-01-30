0e49416cf97377821175320f07aff3020732cf47
Kish-Siva-1 <rodes88@msn.com>
2020-01-30 16:01:34 +0100

    will add info to contact me

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -4,13 +4,17 @@ export const Contact = () => {
     return (
         <section className="page-section" id="contact">
             <div className="container text-center">
+                <h4 className="text-secondary mb-4">
+                    Interested in collaborating on new projects, hiring me or just want to grab a coffee?
+                </h4>
 
-            <a className="page-section-heading text-center btn btn-xl btn-outline-secondary text-uppercase mb-0"
-                target="_top"
-                href="mailto:kishandth.sivapalasundaram@hec.edu?Subject=Hey%20Kish!"
+                <a className="page-section-heading text-center btn btn-xl btn-outline-secondary text-uppercase mb-0"
+                    target="_top"
+                    href="mailto:kishandth.sivapalasundaram@hec.edu?Subject=Hey%20Kish!"
                 >Contact Me</a>
-
             </div>
+
+
         </section>
     );
 };
