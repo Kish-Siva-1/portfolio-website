59c7ed466a922d2c2050b6284835e33311c5edfe
Kish-Siva-1 <rodes88@msn.com>
2020-02-11 16:26:18 +0100

    Updates

---
+++ precache-manifest.f028418e5765c1b874dbf13f0cef2f1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc52a5c686cfed669f8a742bd3003db",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "59b424a508bd75365bb7",
+    "url": "/portfolio-website/static/css/main.fca1e99b.chunk.css"
+  },
+  {
+    "revision": "b7cbfa5cc8a02aadc17b",
+    "url": "/portfolio-website/static/js/2.3e2b5d3e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio-website/static/js/2.3e2b5d3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "59b424a508bd75365bb7",
+    "url": "/portfolio-website/static/js/main.faaad9d5.chunk.js"
+  },
+  {
+    "revision": "8602db4a7a04e93fc5d1",
+    "url": "/portfolio-website/static/js/runtime-main.087a6ff3.js"
+  }
+]);
