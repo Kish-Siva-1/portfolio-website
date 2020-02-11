6f0723f16b2d0065636076e0fa198aef97291e52
Kish-Siva-1 <rodes88@msn.com>
2020-02-11 14:14:02 +0100

    will add hover functions over modals

--- src/components/Portfolio/Stack.js
+++ src/components/Portfolio/Stack.js
@@ -1,21 +1,11 @@
 import React, { useState } from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
+import "./Stack.css"
 
 library.add(fab);
 
 export const Stack = props => {
-    const [activeClass, setActiveClass] = useState("technology");
-
-    const onMouseEnterHandler = () => {
-        setActiveClass("technology-active");
-        props.setHover(true)
-    };
-
-    const onMouseLeaveHandler = () => {
-        setActiveClass("technology");
-        props.setHover(false)
-    };
 
     const addDefaultSrc = (ev) => {
         ev.target.src = `./assets/img/logos/${props.tech}.png`
@@ -23,10 +13,9 @@ export const Stack = props => {
 
     return (
         <div
-            className={`d-flex pb-5 align-items-center ${activeClass} ${props.hover && activeClass === "technology" ? "technology-blur" : ""}`}
+            id="icon"
+            className={`d-flex pb-5 align-items-center`}
             style={{ maxHeight: "60px", maxWidth: "60px" }}
-            onMouseEnter={onMouseEnterHandler}
-            onMouseLeave={onMouseLeaveHandler}
         >
         <figure>
             <img
